99a014121f14434c52e44fa654858ab654bca715
Kevin Wang <kevinwang0324@gmail.com>
2019-02-19 10:59:02 -0800

    display rectangle for last recording session

---
+++ src/DataLoader.java
@@ -0,0 +1,14 @@
+import java.io.File;
+
+public class DataLoader {
+    private File[] allFiles;
+
+    public DataLoader(File[] files) {
+        allFiles = files;
+    }
+
+    public boolean loadData() {
+        return false;
+    }
+
+}
