2b759a50878c94616136b941918ad92d87125543
Roshan Prabhakar <roshan@aprabhakar.com>
2019-02-22 14:24:18 -0800

    created DropDownMenu object

--- src/Canvas.java
+++ src/Canvas.java
@@ -1,47 +1,33 @@
 import javax.swing.*;
-import java.awt.*;
-import java.util.ArrayList;
 
 public class Canvas {
 
     private JPanel panel;
     private JFrame frame;
+
     private QGImage image;
-
-    private ArrayList<String> labels;
-
     private JTextField textField;
-    String textInput;
+    private DropDownMenu menu;
 
     public Canvas(QGImage image, String name) {
-        this.panel = new JPanel();
+
         this.image = image;
+        this.textField = new JTextField();
+        this.menu = new DropDownMenu();
+
         this.frame = new JFrame(name);
-        this.labels = new ArrayList<>();
+        this.panel = new JPanel();
 
-        this.textField = new JTextField("new label");
-        textInput = textField.getText();
-
-        JLabel labelFromImage = new JLabel(image.getIcon(), JLabel.RIGHT);
-        labelFromImage.setVisible(true);
-
-        panel.add(labelFromImage);
+        panel.add(new JLabel(image.getIcon()));
         panel.add(textField);
-
-        panel.setVisible(true);
     }
 
     public void addLabel(String label) {
     }
 
-    public ArrayList<String> display() {
+    public void display() {
         frame.add(panel);
         frame.pack();
         frame.setVisible(true);
-        String[] labels = textInput.split(", ");
-        for (String label : labels) {
-            this.labels.add(label);
-        }
-        return this.labels;
     }
 }
